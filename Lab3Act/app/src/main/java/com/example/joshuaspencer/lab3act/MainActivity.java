910075ad95980aebf2b76cadbe1aeb0ecdb74c9c
love2jn <love2jn@dukes.jmu.edu>
2015-02-13 15:20:33 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    
    	Lab3Act/app/src/main/java/com/example/joshuaspencer/lab3act/MainActivity.java

--- Lab3Act/app/src/main/java/com/example/joshuaspencer/lab3act/MainActivity.java
+++ Lab3Act/app/src/main/java/com/example/joshuaspencer/lab3act/MainActivity.java
@@ -1,73 +1,53 @@
-package com.example.joshuaspencer.lab3act;
 
+package com.example.joshuaspencer.lab3act;
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.provider.MediaStore;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
-
-public class MainActivity extends ActionBarActivity {
-
-    public static final int RC_TAKE_CAMERA_IMG = 0;
-    public static final int RC_CROP = 2;
-    public static final int CAMERA_IMAGE_ACTIVITY_REQUEST_CODE = 111;
-
+import android.widget.ImageView;
+/**
+ * Lab 3
+ * Douglas Applegate
+ * Nick Love(is cool)
+ * Dave McVicar
+ * Joshua Spencer
+ */
+public class MainActivity extends Activity {
+    public static final int REQUEST_CODE_TAKE_FROM_CAMERA = 0;
+    public static final int REQUEST_CODE_CROP_PHOTO = 2;
+    private static final String IMAGE_UNSPECIFIED = "image/*";
+    private static final String URI_INSTANCE_STATE_KEY = "saved_uri";
+    private Uri imageCaptureUri;
+    private ImageView mImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+//Saving image uri before activity goes into bground
+        outState.putParcelable(URI_INSTANCE_STATE_KEY, imageCaptureUri);
     }
-
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void changePicture(View view)
-    {
-
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(cameraIntent, CAMERA_IMAGE_ACTIVITY_REQUEST_CODE);
-    }
-
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if( resultCode != RESULT_OK)
+        if(resultCode != RESULT_OK)
             return;
-
-        switch(requestCode){
-            case RC_CROP:
-
+        switch (requestCode){
+            case REQUEST_CODE_TAKE_FROM_CAMERA:
+//cropPhoto(); //Send picture taken to be cropped
+                break;
+            case REQUEST_CODE_CROP_PHOTO:
+                break;
         }
     }
-
-    public void saveProfile(View view)
-    {
-
-    }
-
-    public void cancelProfile(View view)
-    {
-
-    }
+/*on button clicks*/
+/*public void onChangePictureClick(View view){
+}*/
+/*Private Helpers*/
 }
