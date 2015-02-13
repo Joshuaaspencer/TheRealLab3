d00b9837b179c069ff6decce17a79f70714ac6ef
love2jn <love2jn@dukes.jmu.edu>
2015-02-13 15:40:58 -0500

    goGOPOwAHrANGEAHRS

--- Lab3Act/app/src/main/java/com/example/joshuaspencer/lab3act/MainActivity.java
+++ Lab3Act/app/src/main/java/com/example/joshuaspencer/lab3act/MainActivity.java
@@ -1,29 +1,42 @@
 package com.example.joshuaspencer.lab3act;
-
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
-public class MainActivity extends ActionBarActivity {
+/**
+ * Lab 3
+ * Douglas Applegate
+ * Nick Love(is cool)
+ * Dave McVicar
+ * Joshua Spencer
+ */
 
-    public static final int RC_TAKE_CAMERA_IMG = 0;
-    public static final int RC_CROP = 2;
-    public static final int CAMERA_IMAGE_ACTIVITY_REQUEST_CODE = 111;
-
+public class MainActivity extends Activity {
+    public static final int REQUEST_CODE_TAKE_FROM_CAMERA = 0;
+    public static final int REQUEST_CODE_CROP_PHOTO = 2;
     private static final String IMAGE_UNSPECIFIED = "image/*";
     private static final String URI_INSTANCE_STATE_KEY = "saved_uri";
-
-    private Uri mImageCaptureUri;
+    private Uri imageCaptureUri;
     private ImageView mImageView;
-    private boolean isTakenFromCamera;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
+    /*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,90 +51,72 @@ public class MainActivity extends ActionBarActivity {
 
         //loadSnap();
     }
+    */
 
-    /*
-    We may need to make a few changes or make new methods for the user profile questions
-    --------------------------------This is load snap might want to use this------------------------
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //Saving image uri before activity goes into bground
+        outState.putParcelable(URI_INSTANCE_STATE_KEY, imageCaptureUri);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode != RESULT_OK)
+            return;
+        switch (requestCode){
+            case REQUEST_CODE_TAKE_FROM_CAMERA:
+                //cropPhoto(); //Send picture taken to be cropped
+                break;
+            case REQUEST_CODE_CROP_PHOTO:
+                break;
+        }
+    }
+
     private void loadSnap() {
 
 
-		// Load profile photo from internal storage
-		try {
-			FileInputStream fis = openFileInput(getString(R.string.profile_photo_file_name));
-			Bitmap bmap = BitmapFactory.decodeStream(fis);
-			mImageView.setImageBitmap(bmap);
-			fis.close();
-		} catch (IOException e) {
-			// Default profile photo if no photo saved before.
-			mImageView.setImageResource(R.drawable.default_profile);
-		}
-	}
-	---------------------------Save Snap may be good too---------------------------------------------
-	private void saveSnap() {
-
-	// Commit all the changes into preference file
-		// Save profile image into internal storage.
-		mImageView.buildDrawingCache();
-		Bitmap bmap = mImageView.getDrawingCache();
-		try {
-			FileOutputStream fos = openFileOutput(
-					getString(R.string.profile_photo_file_name), MODE_PRIVATE);
-			bmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
-			fos.flush();
-			fos.close();
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
-	}
-     */
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void changePicture(View view)
-    {
-
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(cameraIntent, CAMERA_IMAGE_ACTIVITY_REQUEST_CODE);
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if( resultCode != RESULT_OK)
-            return;
-
-        switch(requestCode){
-            case RC_CROP:
-
+        // Load profile photo from internal storage
+        try {
+            FileInputStream fis = openFileInput(getString(R.string.profile_photo_file_name));
+            Bitmap bmap = BitmapFactory.decodeStream(fis);
+            mImageView.setImageBitmap(bmap);
+            fis.close();
+        } catch (IOException e) {
+            // Default profile photo if no photo saved before.
+            mImageView.setImageResource(R.drawable.photo);
         }
     }
+    private void saveSnap() {
 
-    public void saveProfile(View view)
-    {
-
+        // Commit all the changes into preference file
+        // Save profile image into internal storage.
+        mImageView.buildDrawingCache();
+        Bitmap bmap = mImageView.getDrawingCache();
+        try {
+            FileOutputStream fos = openFileOutput(
+                    getString(R.string.profile_photo_file_name), MODE_PRIVATE);
+            bmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.flush();
+            fos.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
     }
+/*on button clicks*/
 
-    public void cancelProfile(View view)
-    {
+    /*public void onChangePictureClick(View view){
+    }*/
 
-    }
+/*Private Helpers*/
 }
+
+
+
+
+
+
+
+
+
+
+
