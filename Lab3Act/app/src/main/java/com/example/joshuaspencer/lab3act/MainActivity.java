ddb9b6f972e814a9b696a01ac1988c7cdac2e0b7
love2jn <love2jn@dukes.jmu.edu>
2015-02-12 19:25:31 -0500

    Nick's partial BE changes

--- Lab3Act/app/src/main/java/com/example/joshuaspencer/lab3act/MainActivity.java
+++ Lab3Act/app/src/main/java/com/example/joshuaspencer/lab3act/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.joshuaspencer.lab3act;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public static final int RC_TAKE_CAMERA_IMG = 0;
+    public static final int RC_CROP = 2;
+    public static final int CAMERA_IMAGE_ACTIVITY_REQUEST_CODE = 111;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +42,32 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void changePicture(View view)
+    {
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_IMAGE_ACTIVITY_REQUEST_CODE);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if( resultCode != RESULT_OK)
+            return;
+
+        switch(requestCode){
+            case RC_CROP:
+
+        }
+    }
+
+    public void saveProfile(View view)
+    {
+
+    }
+
+    public void cancelProfile(View view)
+    {
+
     }
 }
